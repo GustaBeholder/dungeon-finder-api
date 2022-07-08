0847464fba9bfa9d4662b6dba66a6b3ba83e4e92
Gustavo Soares <gustavo_hps111@hotmail.com>
2022-07-08 15:53:00 -0300

    Começo tela login

---
+++ DungeonFinderWebApp/Controllers/UsuarioController.cs
@@ -0,0 +1,14 @@
+﻿using DungeonFinderWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DungeonFinderWebApp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Login()
+        {
+            UsuarioLogin login = new UsuarioLogin();
+            return View(login);
+        }
+    }
+}
---
+++ DungeonFinderWebApp/Models/UsuarioLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace DungeonFinderWebApp.Models
+{
+    public class UsuarioLogin
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
